e849b47c603d4e9609934d1630dfcb82183cc52e
Alexey Usov <usov@mercury.office>
2019-08-08 19:03:00 +0400

    changed userRouter's prefix

--- src/app/routes/user-router.ts
+++ src/app/routes/user-router.ts
@@ -5,28 +5,28 @@ import {Validate} from '../middlewares'
 import { userValidators } from '../validators';
 
 const routerOpts: Router.IRouterOptions = {
-  prefix: '/user',
+  prefix: '/api/v1/user',
 };
 
 const router: Router = new Router(routerOpts);
 
 router.get('/', UserController.getAll);
 
-router.get('/:id', async (ctx: Koa.Context) => {
-  ctx.body = 'GET SINGLE';
-});
+// router.get('/:id', async (ctx: Koa.Context) => {
+//   ctx.body = 'GET SINGLE';
+// });
 
 router.post('/',
   Validate({ request: { body: userValidators.createUser } }),
   UserController.addUser
 );
 
-router.delete('/:id', async (ctx: Koa.Context) => {
-  ctx.body = 'DELETE';
-});
+// router.delete('/:id', async (ctx: Koa.Context) => {
+//   ctx.body = 'DELETE';
+// });
 
-router.patch('/:id', async (ctx: Koa.Context) => {
-  ctx.body = 'PATCH';
-});
+// router.patch('/:id', async (ctx: Koa.Context) => {
+//   ctx.body = 'PATCH';
+// });
 
 export default router;
